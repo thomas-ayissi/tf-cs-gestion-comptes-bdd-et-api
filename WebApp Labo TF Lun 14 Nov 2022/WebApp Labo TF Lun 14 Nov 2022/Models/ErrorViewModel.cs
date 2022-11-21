40c3f30b8f095558c32716577f208123362ad207
Thomas Ayissi <thomas8sea@gmail.com>
2022-11-21 15:38:59 +0100

    add all files - maybe after, i can put some in a git ignore

---
+++ WebApp Labo TF Lun 14 Nov 2022/WebApp Labo TF Lun 14 Nov 2022/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApp_Labo_TF_Lun_14_Nov_2022.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
